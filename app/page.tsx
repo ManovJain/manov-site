56822ca19437c79c1e0faa99f2608448e231fc47
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2024-11-26 15:34:03 -0600

    lint build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import styles from "./page.module.css";
 import { ProjectShowcase } from "./components/ProjectShowcase/ProjectShowcase";
 import { Nav } from "./components/Nav/Nav";
