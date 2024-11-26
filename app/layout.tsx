6aad54dfddb5370fe5c70470776613e0d0b30ebf
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2024-11-26 15:32:39 -0600

    SEO and sharing polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,60 @@ const satoshi = localFont({
 });
 
 export const metadata = {
-  title: "Manov Jain - Personal Site",
-  description: "Software engineer and designer based in Seattle",
+  title: {
+    default: "Manov Jain | Software Engineer & Designer",
+    template: "%s | Manov Jain",
+  },
+  description:
+    "Manov Jain is a software engineer and designer based in Seattle, WA. Currently building innovative products at AT&T and working on projects like anim and inSZN.",
+  keywords: [
+    "Manov Jain",
+    "Software Engineer",
+    "Designer",
+    "Seattle",
+    "AT&T",
+    "Web Development",
+    "React",
+    "Next.js",
+  ],
+  authors: [{ name: "Manov Jain" }],
+  creator: "Manov Jain",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://manov.dev",
+    title: "Manov Jain | Software Engineer & Designer",
+    description:
+      "Manov Jain is a software engineer and designer based in Seattle, WA. Currently building innovative products at AT&T and working on projects like anim and inSZN.",
+    siteName: "Manov Jain",
+    images: [
+      {
+        url: "/manov.png",
+        width: 800,
+        height: 800,
+        alt: "Manov Jain",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Manov Jain | Software Engineer & Designer",
+    description:
+      "Software engineer and designer based in Seattle, building innovative products and experiences.",
+    creator: "@ManovJain",
+    images: ["/manov.png"],
+  },
+  icons: {
+    icon: "/favicon.svg",
+  },
+  metadataBase: new URL("https://manov.dev"),
+  appleWebApp: {
+    title: "Manov Jain",
+    statusBarStyle: "default",
+  },
+  alternates: {
+    canonical: "/",
+  },
 };
 
 export default function RootLayout({
@@ -20,7 +72,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body className={satoshi.variable}>
         <Providers>{children}</Providers>
       </body>
